67bfc7e3f4f51a7faacfdf3e8c0e7736f3aed41d
IngenieroCesar <ceagarzonpu@unal.edu.co>
2020-03-16 22:57:55 -0500

    Class 3: Components types

---
+++ src/components/Presentational.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//Aqui no es necesario que yo haga un return explicito y encapsule 
+//lo que quiero enviar, lo hacemos mediante la funcion del arrow function!
+
+const Presentational = () => <h1>Hola Mundo</h1>
+
+export default Presentational;
---
+++ src/components/Stateless.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//Este componente no contiene estado ni ciclo de vida, 
+//simplemente se usa para renderizar.
+
+const Stateless = () => {
+    return(
+        <h1>Hola Mundo</h1>
+    );
+};
+
+export default Stateless;
